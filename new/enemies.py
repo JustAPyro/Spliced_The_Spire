dadd0508199f11f0f166ee132aa4fd2267ac2eed
Luke <Luke.m.hanna@gmail.com>
2023-06-16 13:16:33 -0400

    set up basic ability_patterns

--- new/enemies.py
+++ new/enemies.py
@@ -2,9 +2,11 @@ from abc import ABC, abstractmethod
 from effects import EffectMixin, CURLUP, VULNERABLE
 from random import randint
 from functools import partial
-from typing import TYPE_CHECKING, Dict, Tuple
+from typing import TYPE_CHECKING, Dict, Tuple, Optional
 
+from abstraction_enforcement import protect
 from AscensionMapping import AscensionBasedInt
+from new.ability_patterns import AbilityGenerator
 
 if TYPE_CHECKING:
     from intents import Intent
@@ -31,9 +33,6 @@ class AbstractEnemy(ABC, EffectMixin):
 
         super().__init__()
 
-    def action(self, target, action):
-        action.do(self, target)
-
     def is_dead(self):
         return self.health <= 0
 
@@ -56,13 +55,17 @@ class AbstractEnemy(ABC, EffectMixin):
     def deal_damage(self, damage: int):
         pass
 
-    def set_start(self, health, effects, intent):
+    def set_start(self, health, effects=None, intent=None):
         # TODO: Assert beginning
         self.max_health = health
         self.health = health
         self.clear_effects()
-        self.apply_all_effects(effects)
-        self.intent = intent
+
+        if effects not None:
+            self.apply_all_effects(effects)
+
+        if intent not None:
+            self.intent = intent
         return self
 
     def before_ability(self):
@@ -83,6 +86,29 @@ class AbstractEnemy(ABC, EffectMixin):
 
         return wrapper
 
+    @staticmethod
+    def ability_pattern(func):
+        def wrapper(*args, **kwargs):
+            self = args[0]
+            func(*args, **kwargs)
+
+        return wrapper
+
+
+class Cultist(AbstractEnemy):
+    max_health_range = {
+        0: (48, 54),  # A1- Health is 48-54
+        7: (50, 56)  # A7+ Health is 50-56
+    }
+
+    def __init__(self, ascension=0):
+        super().__init__(name='Cultist',
+                         max_health=Cultist.max_health_range,
+                         ascension=ascension,
+                         act=1)
+
+
+
 
 class RedLouse(AbstractEnemy):
     max_health_range = {
@@ -90,17 +116,21 @@ class RedLouse(AbstractEnemy):
         7: (11, 16)
     }
     curl_up_power = {
-        0: (3, 7),
-        7: (4, 8),
+        +0: (3, 7),
+        +7: (4, 8),
         17: (9, 12)
     }
-    bite_damage_boost = {
+    bite_value = {
         0: 0,  # Deals D + 0 damage on A0
         2: 1,  # Deals D + 1 damage on A2+
     }
+    grow_value = {
+        +0: 3,
+        17: 4,
+    }
 
     def __init__(self, ascension=0):
-        super().__init__(name="Louse",
+        super().__init__(name='Louse',
                          max_health=RedLouse.max_health_range,
                          ascension=ascension,
                          act=1)
@@ -110,18 +140,25 @@ class RedLouse(AbstractEnemy):
 
         # TODO: Confirm this? "Between 5  and 7" Inclusive or exclusive?
         self.damage = randint(5, 7)
-        self.bite_damage = {
-            0: ()
-        }
 
     @AbstractEnemy.ability
-    def bite(self, target=None, damage=None):
+    def bite(self, target=None, damage: Optional[int] = None):
         target.take_damage(damage if damage is not None else
-                           self.damage + AscensionBasedInt(
-                               self.ascension,
-                               RedLouse.bite_damage_boost
-                           ))
+                           self.damage + AscensionBasedInt(self.ascension, RedLouse.bite_value))
 
     @AbstractEnemy.ability
-    def grow(self, quantity):
-        self.apply_strength(quantity)
+    def grow(self, quantity: Optional[int] = None):
+        self.apply_strength(quantity if quantity is not None else
+                            AscensionBasedInt(self.ascension, RedLouse.grow_value))
+
+    @AbstractEnemy.ability_pattern
+    def pattern(self):
+        ag = AbilityGenerator()
+        ag.condition(PercentChance(25, self.grow))
+
+        NotInARow(3, (self.grow, self.bite))
+        Percent(25, self.grow),
+        Percent(75, self.bite)
+
+        if self.ascension > 17:
+            NotInARow(2, self.grow)
