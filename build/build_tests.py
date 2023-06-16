c0bb176e092ebce56e3eb9cd5add1f678ef3f75b
Luke <Luke.m.hanna@gmail.com>
2023-06-16 01:45:17 -0400

    started building automated test system

---
+++ build/build_tests.py
@@ -0,0 +1,4 @@
+from tests.test_framework import *
+
+for card in (RedStrike,):
+    STSTestSuite(card).create()
---
+++ tests/testing.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+
+class TestType(Enum):
+    DEALS_DAMAGE = 0
