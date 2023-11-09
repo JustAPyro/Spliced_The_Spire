c0888af18b4861009eaaa5c1d699449d425a319b
djbee3093 <77656824+djbee3093@users.noreply.github.com>
2023-11-09 17:00:24 -0500

    Broken. Started implementing multiple enemies.

--- game.py
+++ game.py
@@ -6,31 +6,49 @@ from lutil import C
 
 
 class Simulation:
-    def __init__(self, actor: type[AbstractActor], enemies: AbstractEnemy, hero, relics, deck, ascension):
+    def __init__(self, actor: type[AbstractActor], enemies: list[AbstractEnemy], hero, relics, deck, ascension):
         self.actor = actor(hero, cards=deck)
         self.enemies = enemies
 
     def run(self):
         print("Starting Simulation:")
-        print(f'Fighting {self.enemies.name} with {self.enemies.health} health')
-        while self.actor.health > 0 and self.enemies.health > 0:
+        print(f'Fighting {self.get_names()} with {self.get_healths()} health')
+        while self.actor.health > 0:
             turn_log = self.actor.turn_impl(self.actor.hand_pile, self.enemies, verbose=True)
 
-            if self.enemies.health <= 0:
+            if self.enemies_dead():
                 break
 
-            print(f'{C.RED}Cultists turn! Health: {self.enemies.health}, effects: {self.enemies.get_effects_dict()}')
+            print(f'{C.RED}Cultists turn! Health: {self.get_healths()}, effects: {self.enemies.get_effects_dict()}')
             turn = self.enemies.take_turn(self.actor)
-            print(f'{C.RED}{self.enemies.name} {turn}{C.END}')
+            print(f'{C.RED}{self.get_names()} {turn}{C.END}')
 
         if self.actor.health <= 0:
             print("Actor LOST")
         else:
             print("Actor WON")
 
+    def enemies_dead(self):
+        for enemy in self.enemies:
+            if enemy.health >= 0:
+                return False
+        return True
+
+    def get_names(self):
+        names = []
+        for enemy in self.enemies:
+            names.append(enemy.name)
+        return names
+
+    def get_healths(self):
+        healths = []
+        for enemy in self.enemies:
+            healths.append(enemy.health)
+        return healths
+
 
 sim = Simulation(actor=LeftToRightAI,
-                 enemies=Cultist(),
+                 enemies=[Cultist(), Cultist()],
                  hero=Ironclad,
                  relics=[Ironclad.start_relic],
                  deck=[RedDefend(), Clash(), Clash(), RedStrike(), RedStrike(), RedStrike()],
