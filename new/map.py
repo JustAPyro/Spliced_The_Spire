e6fafee1dd03f0dcb497229117e40d2e93f2f9ae
djbee3093 <77656824+djbee3093@users.noreply.github.com>
2023-11-14 15:43:00 -0500

    Changed map to Digraph, added room attributes for floors 1 and 9, implemented get_uncrossed_successors().

--- new/map.py
+++ new/map.py
@@ -1,9 +1,26 @@
 import random
 import networkx as nx
 from matplotlib import pyplot as plt
+from networkx import DiGraph
 
 G = nx.grid_2d_graph(7, 15)
 G.clear_edges()
+D = DiGraph(G)
+
+
+def get_uncrossed_successors(node, potential_successors):
+    uncrossed = []
+    for potential in potential_successors:
+        if potential[0] == node[0]:
+            uncrossed.append(potential)
+        if potential[0] == node[0] - 1:
+            if (node[0], node[1] + 1) not in D.successors((node[0] - 1, node[1])):
+                uncrossed.append(potential)
+        if potential[0] == node[0] + 1:
+            if (node[0], node[1] + 1) not in D.successors((node[0] + 1, node[1])):
+                uncrossed.append(potential)
+
+    return uncrossed
 
 
 def get_possible_successors(node):
@@ -11,16 +28,17 @@ def get_possible_successors(node):
     if floor > 14:
         raise RuntimeError("Tried to find successors for room on top floor")
     if node[0] == 0:
-        return [(0, floor), (1, floor)]
+        return get_uncrossed_successors(node, [(0, floor), (1, floor)])
     if node[0] == 6:
-        return [(5, floor), (6, floor)]
-    return [(node[0] - 1, floor), (node[0], floor), (node[0] + 1, floor)]
+        return get_uncrossed_successors(node, [(5, floor), (6, floor)])
+    return get_uncrossed_successors(node,
+            [(node[0] - 1, floor), (node[0], floor), (node[0] + 1, floor)])
 
 
 def generate_random_path(node):
     if node[1] < 14:
         choice = random.choice(get_possible_successors(node))
-        G.add_edge(node, choice)
+        D.add_edge(node, choice)
         generate_random_path(choice)
 
 
@@ -35,12 +53,21 @@ for _ in range(4):
     generate_random_path((start, 0))
 
 # Casted to a list to get around a weird error (per guy on stackexchange)
-G.remove_nodes_from(list(nx.isolates(G)))
+D.remove_nodes_from(list(nx.isolates(D)))
+
+for node in D.nodes:
+    if node[1] == 0:
+        D.nodes[node]["room"] = "Monster"
+    if node[1] == 8:
+        D.nodes[node]["room"] = "Treasure"
+print(D.nodes(data=True))
 
 plt.figure(figsize=(6, 6))
-pos = {(x, y): (x, y) for x, y in G.nodes()}
-nx.draw(G, pos=pos,
+pos = {(x, y): (x, y) for x, y in D.nodes()}
+labels = nx.get_node_attributes(D, 'room')
+nx.draw(D, pos=pos,
         node_color='lightgreen',
         with_labels=True,
-        node_size=600)
+        node_size=600,
+        labels=labels)
 plt.show()
