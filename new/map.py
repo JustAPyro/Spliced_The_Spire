e6ee5518a5a64e6f21826b9fbbd63292779ef0ef
djbee3093 <77656824+djbee3093@users.noreply.github.com>
2023-11-14 00:47:48 -0500

    Map generates random path starting with random floor 0 room.

--- new/map.py
+++ new/map.py
@@ -1,30 +1,31 @@
+import random
 import networkx as nx
 from matplotlib import pyplot as plt
 
-
 G = nx.grid_2d_graph(7, 15)
-
-
 G.clear_edges()
 
-# G.add_edge((0, 0), (1, 1))
-# G.add_edge((1, 1), (1, 2))
-# G.add_edge((1, 2), (1, 3))
-# G.add_edge((1, 3), (2, 4))
-# G.add_edge((2, 4), (1, 5))
-# G.add_edge((1, 5), (2, 6))
-# G.add_edge((2, 6), (3, 7))
-# G.add_edge((3, 7), (3, 8))
-# G.add_edge((3, 8), (2, 9))
-# G.add_edge((2, 9), (3, 10))
-# G.add_edge((3, 10), (4, 11))
-# G.add_edge((4, 11), (4, 12))
-# G.add_edge((4, 12), (3, 13))
-# G.add_edge((3, 13), (4, 14))
+
+def get_possible_successors(node):
+    floor = node[1] + 1
+    if floor > 14:
+        raise RuntimeError("Tried to find successors for room on top floor")
+    if node[0] == 0:
+        return [(0, floor), (1, floor)]
+    if node[0] == 6:
+        return [(5, floor), (6, floor)]
+    return [(node[0] - 1, floor), (node[0], floor), (node[0] + 1, floor)]
 
 
+def generate_random_path(node):
+    if node[1] < 14:
+        choice = random.choice(get_possible_successors(node))
+        G.add_edge(node, choice)
+        generate_random_path(choice)
 
-print(G.edges)
+
+start = random.randint(0, 6)
+generate_random_path((start, 0))
 
 plt.figure(figsize=(6, 6))
 pos = {(x, y): (x, y) for x, y in G.nodes()}
