f5b42ab0202ce178379f9b322972fe53a0717933
djbee3093 <77656824+djbee3093@users.noreply.github.com>
2023-11-15 00:46:10 -0500

    Finished map generation. I think...

--- new/map.py
+++ new/map.py
@@ -32,7 +32,7 @@ def get_possible_successors(node):
     if node[0] == 6:
         return get_uncrossed_successors(node, [(5, floor), (6, floor)])
     return get_uncrossed_successors(node,
-            [(node[0] - 1, floor), (node[0], floor), (node[0] + 1, floor)])
+                                    [(node[0] - 1, floor), (node[0], floor), (node[0] + 1, floor)])
 
 
 def generate_random_path(node):
@@ -55,12 +55,95 @@ for _ in range(4):
 # Casted to a list to get around a weird error (per guy on stackexchange)
 D.remove_nodes_from(list(nx.isolates(D)))
 
+unfilled_nodes = []
 for node in D.nodes:
     if node[1] == 0:
         D.nodes[node]["room"] = "Monster"
-    if node[1] == 8:
+    elif node[1] == 8:
         D.nodes[node]["room"] = "Treasure"
-print(D.nodes(data=True))
+    elif node[1] == 14:
+        D.nodes[node]["room"] = "Rest"
+    else:
+        unfilled_nodes.append(node)
+
+room_bucket = []
+for _ in range(len(unfilled_nodes)):
+    x = random.randint(1, 100)
+    if x <= 45:  # 45% chance
+        room_bucket.append("Monster")
+    elif x <= 67:  # 22% chance
+        room_bucket.append("Event")
+    elif x <= 83:  # 16% chance
+        room_bucket.append("Elite")
+    elif x <= 95:  # 12% chance
+        room_bucket.append("Rest")
+    else:  # 5% chance
+        room_bucket.append("Shop")
+
+random.shuffle(room_bucket)
+
+
+def rule1(node, room_type):
+    return not (node[1] < 5 and (room_type == "Elite" or room_type == "Rest"))
+
+
+def rule2(node, room_type):
+    if room_type == "Elite":
+        for successor in D.successors(node):
+            if D.nodes[successor].get("room") == "Elite":
+                return False
+        for predecessor in D.predecessors(node):
+            if D.nodes[predecessor].get("room") == "Elite":
+                return False
+
+    if room_type == "Shop":
+        for successor in D.successors(node):
+            if D.nodes[successor].get("room") == "Shop":
+                return False
+        for predecessor in D.predecessors(node):
+            if D.nodes[predecessor].get("room") == "Shop":
+                return False
+
+    if room_type == "Rest":
+        for successor in D.successors(node):
+            if D.nodes[successor].get("room") == "Rest":
+                return False
+        for predecessor in D.predecessors(node):
+            if D.nodes[predecessor].get("room") == "Rest":
+                return False
+    return True
+
+
+def rule3(node, room_type):
+    for predecessor in D.predecessors(node):
+        for successor in D.successors(predecessor):
+            if D.nodes[successor].get("room") == room_type:
+                return False
+    return True
+
+
+def rule4(node, room_type):
+    return not (node[1] == 13 and room_type == "Rest")
+
+
+for node in list(unfilled_nodes):
+    i = 0
+    while i < len(room_bucket):
+        if (rule1(node, room_bucket[i]) and rule2(node, room_bucket[i])
+                and rule3(node, room_bucket[i]) and rule4(node, room_bucket[i])):
+            D.nodes[node]["room"] = room_bucket.pop(i)
+            unfilled_nodes.remove(node)
+            break
+        else:
+            i += 1
+
+print(unfilled_nodes)
+
+# This might be needed if something is left unfilled at the end because nothing is left
+# in room_bucket that follows the rules
+if len(unfilled_nodes) > 0:
+    # iterate through unfilled_nodes and fill with whatever passes the rules
+    pass
 
 plt.figure(figsize=(6, 6))
 pos = {(x, y): (x, y) for x, y in D.nodes()}
